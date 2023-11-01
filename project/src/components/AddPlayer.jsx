8eb2f47690d29d3253b19dcb42e36741a4327a2a
evemp01 <119037756+evemp01@users.noreply.github.com>
2023-11-01 14:48:20 +0100

    hej

--- project/src/components/AddPlayer.jsx
+++ project/src/components/AddPlayer.jsx
@@ -1,11 +1,14 @@
-    const AddPlayer() {
-        return(
+    const AddPlayer = ({}) => {
+          return (
             <>    
                 <div className = 'addPlayer'>
                     <h1>Lägg till spelare</h1>
                     <div className = 'newPlayer'></div>
                     <h2>Spelare 1</h2>
                 </div>
-            </>)
-    }
+            </>
+          )
+      }
+      
+        export default AddPlayer
 
