ceb0ebb752efc35ce4cecbeb46b8dffe73a9da70
albinwill3 <albwi979@student.liu.se>
2023-11-07 09:18:08 +0100

    Allt i AddPlayer
    Bara flytta kod basically

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -1,27 +1,12 @@
 import AddPlayer from './components/AddPlayer'
 import './App.css'
-import React, { useState } from 'react';
+import React from 'react';
 
 function App() {
 
-  const [players, setPlayers] = useState([]);
-
-  const addingPlayer = (newPlayer) => {
-    setPlayers([players, newPlayer]);
-  };
-
   return (
   <div className="app_big_div"> 
-    <h1>Lägg till spelare</h1>
-    <AddPlayer onAdd={addingPlayer} />
-      <div>
-        <ul>
-          {players.map((player, index) => (
-            <li key={index}>{player}</li>
-          ))}
-        </ul>
-      </div>
-    
+    <AddPlayer/>
   </div>
   )
 }
