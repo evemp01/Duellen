872cb3d539695c6722b8377f23587ea0096b521a
DubbGee <wilga619@student.liu.se>
2023-11-08 15:37:58 +0100

    More work on jsx and css

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -4,7 +4,6 @@ import React from 'react';
 import Rules from './components/Rules'
 import Who_won from './components/Who_won'
 function App() {
-
   return (
   <div className="app_big_div"> 
     <Who_won/>
--- project/src/components/Who_won.jsx
+++ project/src/components/Who_won.jsx
@@ -1,24 +1,23 @@
 import '../styles/Who_won.css'
-
 const Who_won = () => {
     return (
         <div>
             <div className='align_menu_help'>
-            <div>
-                <button className='helpButton' onClick>?</button>
-            </div>
-            <div>
-            <button className='menuButton' onClick>≡</button>
-            </div>
+                <div>
+                    <button className='helpButton'>?</button>
+                </div>
+                <div>
+                    <button className='menuButton'>≡</button>
+                </div>
             </div>
             <div className='align' >
                 <h1>Vem vann?</h1>
-                <button className='player1Button' onClick>
-                    <p className='playerText'>Spelare 1</p>
+                <button className='player1Button'>
+                    Spelare 1
                 </button>
-                <h1>vs</h1>
-                <button className='player2Button' onClick>
-                    <p className='playerText'>Spelare 2</p>
+                <h1>eller</h1>
+                <button className='player2Button'>
+                    Spelare 2
                 </button>
             </div>
         </div>
