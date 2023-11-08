f1c3ddf5e7d081b80617bdaa937b7469fbf7d639
albinwill3 <albwi979@student.liu.se>
2023-11-08 15:43:49 +0100

    randomize utmaning funkar
    Ella hjälpte lite oxå

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -1,4 +1,4 @@
-import AddPlayer from './components/AddPlayer'
+import Competition from './components/Competition'
 import './App.css'
 import React from 'react';
 
@@ -6,7 +6,7 @@ function App() {
 
   return (
   <div className="app_big_div"> 
-    <AddPlayer/>
+    <Competition/>
   </div>
   )
 }
--- project/src/components/Competition.jsx
+++ project/src/components/Competition.jsx
@@ -2,9 +2,27 @@ import React from 'react';
 import challengesJson from './../challenges.json';
 
 const Competition = () => {
+
+    const min = 0;
+    const max = challengesJson.allChallenges.length;
     
+    const rand = (min, max) => {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
+    const randNumber = rand(min, max);
+
     return(
-        <div></div>
+        <div className='Competition'>
+            <div id = 'playerOne'>Spelare 1</div>
+            <h1 id = 'VS'>VS</h1>
+            <div id = 'playerTwo'>Spelare 2</div>
+            <div className='utmaning'>
+                <p>{challengesJson.allChallenges[randNumber].challenge}</p>
+            </div>
+            <button className='next'>Nästa</button>
+        </div>
     );
 };
-{/* <h1>{challengesJson.allChallenges[0].challenge}</h1> */}
+
+export default Competition;
