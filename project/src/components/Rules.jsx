223de92671638a6ff9ba63d5443f94445f7e01fc
DubbGee <wilga619@student.liu.se>
2023-11-15 15:05:59 +0100

    small change

--- project/src/components/Rules.jsx
+++ project/src/components/Rules.jsx
@@ -1,17 +1,16 @@
 import '../styles/Rules.css'
-import { ReactDOM } from 'react-dom/client';
 import { Link } from 'react-router-dom';
 
 const Rules = () => {
     return (
         <div>
-            
+
             <div className='alignRight'>
-            <Link to={"/"} >
-                <button className='exitButton'>x</button>
+                <Link to={"/"} >
+                    <button className='exitButton'>x</button>
                 </Link>
             </div>
-           
+
             <div className='align' >
                 <h1>Spelregler</h1>
                 <div className='rulesDiv'>
