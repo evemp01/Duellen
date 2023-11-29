f8ec93fad0e0e755ed4a339878035957cc3e3654
evemp01 <119037756+evemp01@users.noreply.github.com>
2023-11-29 16:42:15 +0100

    hej

--- project/src/components/AddPlayer.jsx
+++ project/src/components/AddPlayer.jsx
@@ -57,6 +57,7 @@ const AddPlayer = ({ players, setPlayers }) => {
           <input
             id="roundedCorners"
             type="text"
+            maxlength="11"
             value={newPlayer}
             onChange={handleInputChange}
             onKeyDown={handleKeyPress}
